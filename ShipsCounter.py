abaf9d53ba3a2a89f7dcb7b3b9ee5ddf883641d2
Drake <69005329+CakeForDrake@users.noreply.github.com>
2021-02-10 19:54:47 +0200

    Update ShipsCounter.py

--- ShipsCounter.py
+++ ShipsCounter.py
@@ -14,16 +14,20 @@ ships = [
 neigh = ((1,0), (-1, 0), (0, -1), (0, 1))
 my_ships = []
 zeroes = []
+
+#Checking cell in list
 def in_arr(i, j):
     
     if 0 <= i < len(ships) and 0<= j < len(ships[0]):    
         return True
 
+#Checking that cell wasn't checked before
 def valid(i, j):
     if not in_arr(i, j):
         return False
     return ships[i][j] and not (i,j) in my_ships
 
+#Checking neighbours cells
 def neighbours(i, j):
     
     for n in neigh:
@@ -36,6 +40,7 @@ def neighbours(i, j):
                 neighbours(x, y)
 
 count = 0
+#Main loop
 for index_x, row in enumerate(ships):
     for index_y, element in enumerate(row):
         
